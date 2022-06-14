9834d8273c0831cf87b0055283dc7985f60f50b5
Tingkai Liu <tingkai.17@intl.zju.edu.cn>
2022-06-14 15:22:24 -0400

    Add empty file for slurm

---
+++ python/ray/autoscaler/_private/slurm/__init__.py
@@ -0,0 +1,21 @@
+'''Ray-Slurm Interface
+
+Created by Tingkai Liu (tingkai2@illinois.edu) on June 10, 2022
+
+'''
+
+ALL_NODES_LIST = []
+HEAD_NODE_IP = ""
+HEAD_NODE_NAME = ""
+
+
+'''Get all the nodes in the cluster
+
+    The global varibles will be filled
+'''
+def init_node_list():
+    pass
+
+
+'''Get the head node name / IP
+'''
